291ead00348aaf984c22cfeafc65878767b35f20
Luís Filipe Martins Loureiro <luisfml@al.insper.edu.br>
2022-03-18 11:17:06 -0300

    updating in syntax

--- main.cpp
+++ main.cpp
@@ -1,19 +1,11 @@
 #include <algorithm>
-#include <initializer_list>
+#include<vector>
 #include <iostream>
 
 using namespace std;
 
-// https://slaystudy.com/initialize-2d-array-c-to-zero/
-void Print2DArray(int arr[][20], int r, int c){
-	for(int i=0;i<r;++i){
-		for(int j=0;j<c;++j){
-			cout<<arr[i][j]<<' ';
-		}	
-		cout<<endl;
-	}
-	cout<<endl;
-}
+typedef vector<vector<int>> matrix;
+
 int calcula_valor(char a, char b){
     if(a==b){
         return 2;
@@ -22,13 +14,24 @@ int calcula_valor(char a, char b){
     }
 }
 
-vector<vector<int>> Heuristica_SW(int n, int m, string a, string b, int w){
-    int del, diag, ins, candidato;
-    int H[n][m] = {0};
+void printador(matrix H, int n, int m){
+
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= m; j++){
+            cout << H[i][j] << "  ";
+        }
+        cout << endl;
+    }
+}
+
+matrix Heuristica_SW(matrix H, int n, int m, string a, string b){
+    int del, diag, ins, candidato,w;
     
 
-    for(int i = 1; i<=n; i++){
-        for(int j; i<=m; i++){
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+
             w = calcula_valor(a[i],b[j]);
             diag = H[i-1][j-1] + w;
             
@@ -38,8 +41,41 @@ vector<vector<int>> Heuristica_SW(int n, int m, string a, string b, int w){
 
             //https://codereview.stackexchange.com/questions/26100/maximum-of-three-values-in-c
             candidato = max({0,diag,del,ins});
+            H[i][j] = candidato;
         }
     }
-    Print2DArray(H,n,m); //printando matriz feita
+   
     return H;
+}
+
+
+int main(){
+
+    int n, m;    
+    string a_OLD, b_OLD;
+
+    cin >> n;
+    cin >> m;
+    cin >> a_OLD;
+    cin >> b_OLD;
+
+    string a = "#" + a_OLD;
+    string b = "#" + b_OLD;
+
+     cout << a << endl << b << endl;
+
+    matrix H;
+    H.resize(n+1);
+    for(int zero = 0; zero <=n; zero++){
+        H[zero].resize(m+1);
+    }
+
+    H = Heuristica_SW(H,n,m,a,b);
+    printador(H,n,m);
+    
+
+    cout << "END";
+    
+    return 0;
+
 }
